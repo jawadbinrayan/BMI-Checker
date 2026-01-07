e5c23ee49eddb5802245a92a88965ecfe02f61ce
jawadbinrayan <rayanslifebd@gmail.com>
2026-01-07 21:44:47 +0600

    Update BMI Checker.py

--- BMI Checker.py
+++ BMI Checker.py
@@ -7,3 +7,6 @@ from sklearn.model_selection import train_test_split
 
 st.title("BMI Checker Web Application")
 st.subheader("Data Science with Jawad Bin Rayan")
+
+# Sidebar
+st.sidebar.header("Upload CSV Data or Use Sample")
