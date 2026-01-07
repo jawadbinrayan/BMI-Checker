3c9c3ed4ddffbdf2d4b895483d4d998bd1269980
jawadbinrayan <rayanslifebd@gmail.com>
2026-01-07 21:40:39 +0600

    Update BMI Checker.py

--- BMI Checker.py
+++ BMI Checker.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 st.title("BMI Checker Web Application")
+st.subheader("Data Science with Jawad Bin Rayan")
