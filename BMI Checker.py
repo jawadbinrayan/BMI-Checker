c94e3c941d44c4134a15f913a0d5bfb3f69e8362
jawadbinrayan <rayanslifebd@gmail.com>
2026-01-07 22:04:03 +0600

    Update BMI Checker.py

--- BMI Checker.py
+++ BMI Checker.py
@@ -1,12 +1,30 @@
-import streamlit as st
-import pandas
-import numpy
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+st.subheader("BMI Checker")
 
-st.title("BMI Checker Web Application")
-st.subheader("Data Science with Jawad Bin Rayan")
+height = st.number_input(
+    "Enter your height (in meters)",
+    min_value=0.5,
+    max_value=3.0,
+    step=0.01
+)
 
-# Sidebar
-st.sidebar.header("Upload CSV Data or Use Sample")
+weight = st.number_input(
+    "Enter your weight (in kilograms)",
+    min_value=10.0,
+    max_value=300.0,
+    step=0.5
+)
+
+if st.button("Calculate BMI"):
+    bmi = weight / (height ** 2)
+    st.write("### Your BMI is:", round(bmi, 2))
+
+    if bmi <= 18.9:
+        st.warning("You are underweight")
+    elif bmi <= 24.4:
+        st.success("You are healthy")
+    elif bmi <= 29.4:
+        st.info("You are overweight")
+    elif bmi <= 34.4:
+        st.error("You are obese")
+    else:
+        st.error("You are severely obese")
